e203a3e1288af7fb2fbf7ab7fa28c8343aadb52a
crocket77 <ethanpfeiffer345@outlook.com>
2022-04-15 13:14:38 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+app.get('*', (req,res)=>{
+    res.sendFile(__dirname +'/public/index.html')
+});
+
+app.get('/notes', (req,res)=>{
+    res.sendFile(__dirname + '/public/notes.html');
+});
+
+app.listen(3001);
