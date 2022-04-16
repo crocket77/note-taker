56d981e9bb6cff11958ceadabb31999caf031a6f
crocket77 <ethanpfeiffer345@outlook.com>
2022-04-16 09:12:08 -0500

    pushing to heroku

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express')
+// const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+const PORT = process.env.PORT || 3001;
 const app = express()
+
+
 
 app.get('*', (req,res)=>{
     res.sendFile(__dirname +'/public/index.html')
@@ -9,4 +14,6 @@ app.get('/notes', (req,res)=>{
     res.sendFile(__dirname + '/public/notes.html');
 });
 
-app.listen(3001);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
